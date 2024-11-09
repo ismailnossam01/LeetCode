2531f4a737aa874753015880fe2547355e153b2b
ismail_nossam_01 <138509028+ismailnossam01@users.noreply.github.com>
2024-11-09 19:19:02 +0530

    Time: 0 ms (100%), Space: 11.5 MB (73.33%) - LeetHub

---
+++ 3394-minimum-array-end/3394-minimum-array-end.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minEnd(self, n, x):
+        result = x
+        remaining = n - 1
+        position = 1
+    
+        while remaining:
+            if not (x & position):
+                result |= (remaining & 1) * position
+                remaining >>= 1
+            position <<= 1
+    
+        return result
+        
